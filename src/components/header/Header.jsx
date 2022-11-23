5fc53cc3e4ee22ac6bea4d17f03c9c01c847c029
MAvi-Rup <avirupmondal19@gmail.comm>
2022-11-23 17:00:47 +0600

    Added Firebase

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,6 +15,10 @@ const headerNav = [
         display: 'Movies',
         path: '/movie'
     },
+    {
+        display:'Login',
+        path:'/login'
+    }
     // {
     //     display: 'TV Series',
     //     path: '/tv'
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuQFuqc8FshlAgvbghNOMi8lyWKeMhuLI",
+  authDomain: "my-movie-application-844ab.firebaseapp.com",
+  projectId: "my-movie-application-844ab",
+  storageBucket: "my-movie-application-844ab.appspot.com",
+  messagingSenderId: "515276611726",
+  appId: "1:515276611726:web:c7da6862fe6908332669df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
