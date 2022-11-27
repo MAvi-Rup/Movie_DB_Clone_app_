913afbd51e9f95e010315d07b5c79dcdb7c26497
MAvi-Rup <avirupmondal19@gmail.comm>
2022-11-27 17:41:25 +0600

    Login Completed

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -59,40 +59,40 @@ const Login = () => {
 
 
     return (
-        <div>
+        <div className='mt-5 pt-5 w-50 m-auto'>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left md:w-1/2">
                         <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Login our website to place an order. And if you like our design and pattern please subscribe our newsletter to get the latest update price and design.</p>
+                        <p className="py-6">Login our website to view the latest movie and TV shows fully free.</p>
                     </div>
-                    <form onSubmit={formSubmit} className="card flex-shrink-0 md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={formSubmit} className="card flex-shrink-0 md:w-1/2 max-w-sm shadow-2xl bg-base-100 mb-5">
                         <div className="card-body">
-                            <div className="form-control">
+                            <div className="form-control mb-3">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" required ref={emailRef} placeholder="Email" className="input input-bordered" />
+                                <input type="email" required ref={emailRef} placeholder="Input your Email" className="input input-bordered bg-white text-dark" />
                             </div>
-                            <div className="form-control">
+                            <div className="form-control mb-3">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" ref={passRef} required placeholder="Password" className="input input-bordered" />
-                                <label className="label">
+                                <input type="password" ref={passRef} required placeholder="Input Your Password" className="input input-bordered bg-white text-dark" />
+                                {/* <label className="label">
                                     <button onClick={resetPassword} className="label-text-alt link link-hover">Forgot password?</button>
-                                </label>
+                                </label> */}
                             </div>
                             {handleError}
-                            <div className="form-control mt-6">
-                                <button className="btn btn-accent text-white">Login</button>
+                            <div className=" mt-2 mb-2">
+                                <button className="btn btn-warning">Login</button>
                             </div>
-                            <p className='text-danger italic'>New to Ceramic Tiles? <Link to="/register" className='text-primary no-underline'>Please Register</Link> </p>
+                            <p className='text-danger italic'>New to MovieDB? <Link to="/register" className='text-primary no-underline'>Please Register</Link> </p>
                             <div className="divider">OR</div>
-                            <button
+                            <div
                                 onClick={() => signInWithGoogle()}
-                                className="btn btn-accent text-white"
-                            >Continue with Google</button>
+                                className="btn btn-warning"
+                            >Continue with Google</div>
                         </div>
 
                     </form>
